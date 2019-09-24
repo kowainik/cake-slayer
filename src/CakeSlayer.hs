c17a0cfbc4b30253e4901585480440f196a00603
Dmitrii Kovanikov <kovanikov@gmail.com>
2019-09-24 09:11:36 +0300

    [#5] Implement application monad
    Resolves #5

--- src/CakeSlayer.hs
+++ src/CakeSlayer.hs
@@ -1,7 +1,9 @@
 module CakeSlayer
        ( module CakeSlayer.Error
        , module CakeSlayer.Has
+       , module CakeSlayer.Monad
        ) where
 
 import CakeSlayer.Error
 import CakeSlayer.Has
+import CakeSlayer.Monad
