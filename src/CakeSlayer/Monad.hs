127beea1f49ce4a3b1d37c4dd2bac1302cf10fc6
Dmitrii Kovanikov <dmitrii@holmusk.com>
2019-09-24 11:59:34 +0300

    [#5] Implement application monad (#15)
    * Update src/CakeSlayer/Monad.hs
    
    Co-Authored-By: Veronika Romashkina <vrom911@gmail.com>
    
    * Update src/CakeSlayer/Monad.hs
    
    Co-Authored-By: Veronika Romashkina <vrom911@gmail.com>

---
+++ src/CakeSlayer/Monad.hs
@@ -0,0 +1,85 @@
+{- | This module implements the main monad for your application.
+
+It should be used in the following way:
+
+1. Create your custom environment data type.
+2. Implement 'CakeSlayer.Has.Has' instances for your environment.
+3. Specialize 'App' to your environment.
+4. Implement desired effects for your specialized version of monad.
+-}
+
+module CakeSlayer.Monad
+       ( App (..)
+       , runApp
+       , runAppAsIO
+       ) where
+
+import Control.Exception (catch, throwIO, try)
+import Control.Monad.Except (MonadError (..))
+import Control.Monad.IO.Unlift (MonadUnliftIO (..))
+import Relude.Extra.Bifunctor (firstF)
+
+import CakeSlayer.Error (AppException (..), ErrorWithSource)
+
+
+{- | Main application monad. It has the following type variables:
+
+* @err@: phantom type varible that represents type of errors thrown by 'App'
+* @env@: application environment that stores settings and in-memory caches
+* @a@: monadic result
+
+Specialize in your code your monomorphic environment data type like in the
+examples below:
+
+@
+__type__ App     = CakeSlayer.'App' AppError  AppEnv
+__type__ MockApp = CakeSlayer.'App' TestError MockEnv
+@
+-}
+newtype App (err :: Type) env a = App
+    { unApp :: ReaderT env IO a
+    } deriving newtype ( Functor, Applicative, Monad, MonadFail
+                       , MonadReader env, MonadIO, MonadUnliftIO)
+
+{- | This instance allows to throw and catch errors that are visible in type
+definitions. The implementation relies on underlying 'IO' machinery.
+
+Use 'CakeSlayer.Error.throwError' and 'CakeSlayer.Error.catchError': these
+functions automatically attach source code positions to errors.
+-}
+instance (Show err, Typeable err)
+    => MonadError (ErrorWithSource err) (App err env)
+  where
+    throwError :: ErrorWithSource err -> App err env a
+    throwError = liftIO . throwIO . AppException
+    {-# INLINE throwError #-}
+
+    catchError
+        :: App err env a
+        -> (ErrorWithSource err -> App err env a)
+        -> App err env a
+    catchError action handler = App $ ReaderT $ \env -> do
+        let ioAction = runApp env action
+        ioAction `catch` \(AppException e) -> runApp env $ handler e
+    {-# INLINE catchError #-}
+
+{- | Run application by providing environment.
+
+Throws 'AppException' if application has unhandled 'throwError'. Use
+'runAppAsIO' to handle exceptions as well.
+-}
+runApp :: env -> App err env a -> IO a
+runApp env = usingReaderT env . unApp
+{-# INLINE runApp #-}
+
+{- | Like 'runApp' but also catches 'AppException' and unwraps 'ErrorWithSource'
+from it. Use this function to handle errors outside 'App' monad.
+-}
+runAppAsIO
+    :: (Show err, Typeable err)
+    => env
+    -> App err env a
+    -> IO (Either (ErrorWithSource err) a)
+runAppAsIO env = firstF unAppException . try . runApp env
+{-# INLINE runAppAsIO #-}
+
