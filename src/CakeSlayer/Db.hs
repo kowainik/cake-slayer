ba55cd8b3d38e9d8eed0e1020370d628459f983a
Dmitrii Kovanikov <kovanikov@gmail.com>
2019-09-30 14:27:00 +0300

    [#35] Add missing bits
    Resolves #35

--- src/CakeSlayer/Db.hs
+++ src/CakeSlayer/Db.hs
@@ -14,15 +14,23 @@ module CakeSlayer.Db
        , queryRaw
        , query
        , queryNamed
+       , queryWithNamed
        , executeRaw
+       , executeRaw_
        , execute
+       , execute_
        , executeMany
+       , executeMany_
        , executeNamed
        , executeNamed_
        , returning
 
          -- * Migrations
        , runMigrations
+
+         -- * Error helpers
+       , asSingleRowWith
+       , failParsingWith
 
          -- * Internal helpers
        , withPool
@@ -31,11 +39,13 @@ module CakeSlayer.Db
 import Database.PostgreSQL.Simple (FromRow, ToRow)
 import PgNamed (NamedParam, PgNamedError)
 
-import CakeSlayer.Error (WithError, liftError)
+import CakeSlayer.Error (WithError, liftError, throwOnNothingM, toNoSourceException)
 import CakeSlayer.Has (Has, grab)
 
 import qualified Data.Pool as Pool
 import qualified Database.PostgreSQL.Simple as Sql
+import qualified Database.PostgreSQL.Simple.FromField as Sql (conversionError)
+import qualified Database.PostgreSQL.Simple.FromRow as Sql
 import qualified Database.PostgreSQL.Simple.Migration as Sql
 import qualified PgNamed as Sql
 
@@ -91,35 +101,75 @@ queryNamed
     -> [NamedParam]
     -> m [res]
 queryNamed q params =
-  withPool (\conn -> runExceptT $ Sql.queryNamed conn q params) >>= liftError
+    withPool (\conn -> runExceptT $ Sql.queryNamed conn q params) >>= liftError
 {-# INLINE queryNamed #-}
 
--- | Executes a query without arguments that is not expected to return results.
+{- | Performs a query with named parameters and custom 'RowParser' and returns a
+list of rows.
+-}
+queryWithNamed
+    :: (WithError PgNamedError m, WithDb env m)
+    => Sql.RowParser res
+    -> Sql.Query
+    -> [NamedParam]
+    -> m [res]
+queryWithNamed rowParser q params =
+    withPool (\conn -> runExceptT $ Sql.queryWithNamed rowParser conn q params) >>= liftError
+{-# INLINE queryWithNamed #-}
+
+-- | Executes a query without arguments that and return a number of affected rows.
 executeRaw
     :: (WithDb env m)
     => Sql.Query
-    -> m ()
-executeRaw q = withPool $ \conn -> void $ Sql.execute_ conn q
+    -> m Int64
+executeRaw q = withPool $ \conn -> Sql.execute_ conn q
 {-# INLINE executeRaw #-}
 
--- | Executes a query with parameters that is not expected to return results.
+-- | Executes a query without arguments that is not expected to return results.
+executeRaw_
+    :: (WithDb env m)
+    => Sql.Query
+    -> m ()
+executeRaw_ = void . executeRaw
+{-# INLINE executeRaw_ #-}
+
+-- | Executes a query with parameters and return a number of affected rows.
 execute
     :: forall args env m .
        (WithDb env m, ToRow args)
     => Sql.Query
     -> args
-    -> m ()
-execute q args = withPool $ \conn -> void $ Sql.execute conn q args
+    -> m Int64
+execute q args = withPool $ \conn -> Sql.execute conn q args
 {-# INLINE execute #-}
 
--- | Executes a multi-row query that is not expected to return results.
+-- | Executes a query with parameters that is not expected to return results.
+execute_
+    :: forall args env m .
+       (WithDb env m, ToRow args)
+    => Sql.Query
+    -> args
+    -> m ()
+execute_ q args = void $ execute q args
+{-# INLINE execute_ #-}
+
+-- | Executes a multi-row query and returns a number of affected rows.
 executeMany
     :: (WithDb env m, ToRow args)
     => Sql.Query
     -> [args]
-    -> m ()
-executeMany q args = withPool $ \conn -> void $ Sql.executeMany conn q args
+    -> m Int64
+executeMany q args = withPool $ \conn -> Sql.executeMany conn q args
 {-# INLINE executeMany #-}
+
+-- | Executes a multi-row query that is not expected to return results.
+executeMany_
+    :: (WithDb env m, ToRow args)
+    => Sql.Query
+    -> [args]
+    -> m ()
+executeMany_ q args = void $ executeMany q args
+{-# INLINE executeMany_ #-}
 
 {- | Executes a query with named parameters, returning the
 number of rows affected
@@ -163,6 +213,22 @@ runMigrations
     => [Sql.MigrationCommand]
     -> m (Sql.MigrationResult String)
 runMigrations commands = withPool $ \conn -> Sql.runMigrations True conn commands
+
+----------------------------------------------------------------------------
+-- Errors
+----------------------------------------------------------------------------
+
+{- | Throws a given error if a monadic action (expected resut of SQL query)
+returns no rows. Otherwise returns the first row.
+-}
+asSingleRowWith :: WithError err m => err -> m [a] -> m a
+asSingleRowWith err action = throwOnNothingM err (viaNonEmpty head <$> action)
+{-# INLINE asSingleRowWith #-}
+
+-- | Helper to throw a given when defining a 'FromRow' instance
+failParsingWith :: (Show err, Typeable err) => err -> Sql.RowParser a
+failParsingWith err =
+    Sql.fieldWith (\_ _ -> Sql.conversionError $ toNoSourceException err)
 
 ----------------------------------------------------------------------------
 -- Helpers
